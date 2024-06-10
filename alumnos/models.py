b516e8c8c9e748fd6a576cac2c8f99df015dc944
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-06-10 18:40:24 -0400

    cambios importantes

--- alumnos/models.py
+++ alumnos/models.py
@@ -21,7 +21,7 @@ class Producto(models.Model):
     nombre_prod = models.CharField(max_length=100)
     descripcion_prod = models.CharField(max_length=100)
     precio = models.IntegerField()
-    imagen = models.ImageField(default='',upload_to='media/')
+    imagen = models.ImageField(default='',upload_to='images/')
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column='id_categoria')
 
     def __str__(self):
--- instituto/settings.py
+++ instituto/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
