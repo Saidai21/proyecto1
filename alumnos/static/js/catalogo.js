e2a39a584354e2459c7e3861cce16a1c8ec770b6
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-06-11 19:53:23 -0400

    changes

--- alumnos/static/js/catalogo.js
+++ alumnos/static/js/catalogo.js
@@ -14,10 +14,9 @@
           });
         });
       });
-      $(document).ready(function(){
-        // Función para manejar el evento click del botón "Mostrar todas las bicicletas"
-        $('#showAllBikes').click(function () {
-          $('.collapse').collapse('show');
+      $(document).ready(function() {
+        $('#showAllBikes').click(function() {
+            $('.collapse').collapse('show');
         });
-      });
+    });
       
