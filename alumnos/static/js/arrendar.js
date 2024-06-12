9f106c5a05e1932816118f919074e9e51b6f1439
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-06-11 22:55:37 -0400

    cmabios arrendar

--- alumnos/static/js/arrendar.js
+++ alumnos/static/js/arrendar.js
@@ -8,7 +8,60 @@ $(document).ready(function(){
             $("#mensajeNombre").fadeIn()
             $("#nombreCliente").css("border-color","red")
             event.preventDefault();
+        }else{
+            $("#mensajeNombre").fadeOut()
+            $("#nombreCliente").css("border-color", "#ced4da")
         }
-
+        if(periodoArriendo==""){
+            $("#mensajePeriodo").fadeIn();
+            $("#periodoArriendo").css("border-color","red")
+        }else{
+            if(periodoArriendo<=0){
+                $("#mensajePeriodo2").fadeIn();
+                $("#periodoArriendo").css("border-color","red")
+            }else{
+                $("#mensajePeriodo2").fadeOut();
+                $("#mensajePeriodo").fadeOut()
+                $("#periodoArriendo").css("border-color", "#ced4da")
+            }
+        }
+        if(formaPago==null){
+            $("#mensajeForma").fadeIn();
+            $("#formaPago").css("border-color","red")
+        }else{
+            $("#mensajeForma").fadeOut();
+            $("#formaPago").css("border-color", "#ced4da")
+        }
+        if(depositoGarantia==""){
+            $("#mensajeDeposito").fadeIn();
+            $("#depositoGarantia").css("border-color","red")
+        }else{
+            if(depositoGarantia<=0){
+                $("#mensajeDeposito2").fadeIn();
+                $("#depositoGarantia").css("border-color","red")
+            }else{
+                $("#mensajeDeposito2").fadeOut();
+                $("#mensajeDeposito").fadeOut()
+                $("#depositoGarantia").css("border-color", "#ced4da")
+            }
+        }
+        $("#nombreCliente").change(function(){
+            $("#nombreCliente").css("border-color", "#ced4da")
+            $("#mensajeNombre").fadeOut()
+        })
+        $("#periodoArriendo").change(function(){
+            $("#mensajePeriodo2").fadeOut();
+            $("#mensajePeriodo").fadeOut()
+            $("#periodoArriendo").css("border-color", "#ced4da")
+        })
+        $("#formaPago").change(function(){
+            $("#mensajeForma").fadeOut();
+            $("#formaPago").css("border-color", "#ced4da")
+        })
+        $("#depositoGarantia").change(function(){
+            $("#mensajeDeposito2").fadeOut();
+            $("#mensajeDeposito").fadeOut()
+            $("#depositoGarantia").css("border-color", "#ced4da")
+        })
     })
 })
