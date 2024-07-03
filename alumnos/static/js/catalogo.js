53cd9d1bf9842a0473ad2fd434c8f3996dbbf2f5
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-07-02 22:07:06 -0400

    implementacion de API

--- alumnos/static/js/catalogo.js
+++ alumnos/static/js/catalogo.js
@@ -20,3 +20,32 @@
         });
     });
       
+    $(document).ready(function() {
+      // Función para obtener las tasas de cambio y actualizar los precios
+      function updatePrices(currency) {
+          $.ajax({
+              url: "https://api.exchangerate-api.com/v4/latest/CLP",
+              success: function(response) {
+                  var rates = response.rates;
+                  var baseRate = rates[currency];
+                  $('.precio').each(function() {
+                      var basePrice = $(this).data('precio');
+                      var newPrice = basePrice * baseRate;
+                      $(this).text('$ ' + newPrice.toFixed(2) + ' ' + currency);
+                  });
+              },
+              error: function() {
+                  alert('Error al obtener las tasas de cambio.');
+              }
+          });
+      }
+
+      // Evento de cambio de moneda
+      $('#moneda').change(function() {
+          var selectedCurrency = $(this).val();
+          updatePrices(selectedCurrency);
+      });
+
+      // Actualizar precios al cargar la página con la moneda por defecto
+      updatePrices($('#moneda').val());
+  });
