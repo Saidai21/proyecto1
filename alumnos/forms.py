4179c421084542f113672183be427d0f2caec103
Saidai21 <max.valdebenit6@gmail.com>
2024-07-05 09:10:49 -0400

    Merge branch 'main' of https://github.com/Saidai21/proyecto1

--- alumnos/forms.py
+++ alumnos/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from django.contrib.auth.models import User
+from .models import Cliente
 
 class UpdateProfileForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, required=False)
-
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+        model = Cliente
+        fields = ['nombre', 'correo', 'contrasena']
+        widgets = {
+            'contrasena': forms.PasswordInput(),
+        }
